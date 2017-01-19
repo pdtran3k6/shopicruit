dbd0e2542f7ae63f365b736da5e9f3a89f3d2338
Phat Tran <phatdtran3k6@gmail.com>
2017-01-18 19:41:39 -0500

    Removed server setup Refactored code

--- app.js
+++ app.js
@@ -1,30 +1,26 @@
 // Import modules
-var restify = require('restify');
 var request = require('request');
 
-// Set up server
-var server = restify.createServer();
-server.listen(3978, function () {
-    console.log('%s listening to %s', server.name, server.url);
-});
+// Variable declaration
+// total_revenue: store revenue for different currency
+// page_num: page number of order.json
+// orders_obj: store the list of orders
+var total_revenue = {};
+var page_num = 1;
+var orders_obj;
 
-// Use bodyParser to get order list from request
-server.use(restify.bodyParser());
-
-// Set up endpoint get_revenue to calculate revenue
-server.post('/get_revenue', function (req, res) {
-    
-    // Deduct tax from price, if tax is included in price
-    function revenueAfterTax(taxes_included, subtotal_price, total_tax) {
-        if (taxes_included) {
-            return parseFloat(subtotal_price - total_tax)
-        } else {
-            return parseFloat(subtotal_price)
-        }
+// Deduct tax from price, if tax is included in price
+function revenueAfterTax(taxes_included, subtotal_price, total_tax) {
+    if (taxes_included) {
+        return parseFloat(subtotal_price - total_tax)
+    } else {
+        return parseFloat(subtotal_price)
     }
-    
+}
+
+function get_revenue(order_json) {
     // Extract list of orders from request
-    var list_of_orders = req.params.orders;
+    var list_of_orders = order_json.orders;
     var revenue = {};
 
     // Iterate through all orders in list_of_orders
@@ -46,44 +42,17 @@ server.post('/get_revenue', function (req, res) {
             list_of_orders[i]["total_tax"])
         }
     }
-    
-    // Respond with a revenue hashtable
-    res.send(revenue);
-    res.status(err ? 500 : 200);
-    if (err) {
-        console.log(err);
-    }
-    res.end();
-});
 
-// Variable declaration
-// total_revenue: store revenue for different currency
-// page_num: page number of order.json
-// orders_obj: store the list of orders
-var total_revenue = {};
-var page_num = 1;
-var orders_obj;
-
-// Send orders_obj to get_revenue endpoint 
-function sendOrders(order) {
-    request.post({
-        url: 'http://localhost:3978/get_revenue',
-        json: order
-    }, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-            
-            // For each currency, add the revenue 
-            // in the response body to totalRevenue 
-            for (var currency in body) {
-                if (currency in total_revenue) {
-                    total_revenue[currency] += body[currency]
-                } else {
-                    total_revenue[currency] = body[currency]
-                }
-            }
+    // For each currency, add the revenue in revenue to totalRevenue 
+    for (var currency in revenue) {
+        if (currency in total_revenue) {
+            total_revenue[currency] += revenue[currency]
+        } else {
+            total_revenue[currency] = revenue[currency]
         }
-    });
-}
+    }
+}  
+
 
 // Check to see if order json has any orders at all
 function extractOrders(page) {
@@ -95,7 +64,7 @@ function extractOrders(page) {
             // If orders exist, get revenue for this order page and take the next order page
             // Otherwise, output total revenue
             if (orders_obj.orders.length > 0) {
-                sendOrders(orders_obj);
+                get_revenue(orders_obj);
                 extractOrders(page + 1);
             } else {
                 console.log("Revenue in all currency:");
